3c91957f305101b767972586d61fab320516da88
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2018-02-13 23:09:13 +0100

    Improve vagrant function

--- home/.config/fish/functions/vagrant.fish
+++ home/.config/fish/functions/vagrant.fish
@@ -1,5 +1,21 @@
-function vagrant
-	pushd ~/vagrant/$argv[1]
-	command vagrant $argv[2..-1]
-	popd
+if command -s vagrant
+	function vagrant
+		set box $argv[1]
+		set args $argv[2..-1]
+		set dir ~/vagrant/$box
+
+		if not test -d $dir
+			echo "$dir does not exist."
+			echo "Available boxes:" (fish -c 'cd ~/vagrant; echo *')
+			read --prompt-str "Create $box? [yN] " reply
+			if test (string lower $reply) != "y"
+				return
+			end
+			mkdir $dir
+		end
+
+		pushd $dir
+		command vagrant $args
+		popd
+	end
 end
