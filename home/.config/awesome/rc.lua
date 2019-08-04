3f87a67154ec7532bee2377c991e06b050328ea1
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-08-04 19:24:24 +0200

    awesome: Restart when screen configuration changes

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -396,6 +396,9 @@ awful.screen.connect_for_each_screen(function(s)
         },
     }
 end)
+
+screen.connect_signal("list", awesome.restart)
+screen.connect_signal("primary_changed", awesome.restart)
 -- }}}
 
 -- {{{ Mouse bindings
